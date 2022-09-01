936eb93d7c46b107adef42a34f177ea6074077f9
Teemu Rintala <teemu.ma.rintala@outlook.com>
2022-09-01 15:30:06 +0300

    adds part 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,6 +18,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {good + neutral + bad}</p>
+      <p>average: {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive: {(100 * good) / (good + neutral + bad)} %</p>
     </div>
   );
 };
