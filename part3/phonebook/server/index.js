6d342ae009d3f635dfd5d01f7e978ab54a9c5daa
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-10-21 22:25:22 +0300

    adds part3.8

--- part3/phonebook/server/index.js
+++ part3/phonebook/server/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token("body", (req) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 const PORT = 3001;
 
