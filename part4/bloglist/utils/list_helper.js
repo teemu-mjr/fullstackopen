daefc7f456abb54f351de8e887d2d81f8172d449
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-11-11 21:46:09 +0200

    add part 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let result = 0;
+  for (let i = 0; i < blogs.length; ++i) {
+    result += blogs[i].likes;
+  }
+  return result;
+};
+
 module.exports = {
   dummy,
+  totalLiked: totalLikes,
 };
