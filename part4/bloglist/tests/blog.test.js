d99c1c20a2bf114a732aea6b2d646ca67f23d413
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2022-12-29 17:55:09 +0200

    add part 4.10

--- part4/bloglist/tests/blog.test.js
+++ part4/bloglist/tests/blog.test.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app.js");
+const api = supertest(app);
+
 const Blog = require("../models/blog");
 
-const api = supertest(app);
+const initialBlogs = [
+  {
+    title: "Test Title",
+    author: "Test Author",
+    url: "test.site.com",
+    likes: 123,
+  },
+  {
+    title: "title",
+    author: "author",
+    url: "url",
+    likes: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObj = new Blog(initialBlogs[0]);
+  await blogObj.save();
+  blogObj = new Blog(initialBlogs[1]);
+  await blogObj.save();
+});
 
 test("blog posts are returned in json", async () => {
   await api
@@ -13,12 +36,38 @@ test("blog posts are returned in json", async () => {
 });
 
 test("blog posts have id instead of _id", async () => {
-  const blog = new Blog({
-    title: "title",
-    author: "author",
-    likes: 0,
-  });
+  const blog = new Blog(initialBlogs[0]);
   expect(blog.toJSON().id).toBeDefined();
+});
+
+test("blog posts are saved on POST", async () => {
+  const newPost = {
+    title: "POST",
+    author: "Jest",
+    url: "test.jest",
+    likes: 1,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newPost)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const res = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const titles = res.body.map((b) => b.title);
+  const authors = res.body.map((b) => b.author);
+  const urls = res.body.map((b) => b.url);
+
+  expect(res.body).toHaveLength(initialBlogs.length + 1);
+
+  expect(titles).toContain(newPost.title);
+  expect(authors).toContain(newPost.author);
+  expect(urls).toContain(newPost.url);
 });
 
 afterAll(() => {
